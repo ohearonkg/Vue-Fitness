bd2338ae2423e8434713b444f514029d4a2dc859
K.G. O'Hearon <ohearonkg@gmail.com>
2018-05-21 18:37:31 -0300

    basic log screen starting.

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,15 +6,21 @@ import Vuex from 'vuex'; // Vue plugins
 // Import your custom components.
 import MyButton from '../src/components/MyButton/MyButton.vue';
 import Modal from '../src/components/Modal/Modal.vue';
+import CategoryList from '../src/components/CategoryList/CategoryList.vue';
+import LogScreen from '../src/components/LogScreen/LogScreen.vue';
 
 // Register custom components.
 Vue.component('my-button', MyButton);
+Vue.component('modal', Modal);
+Vue.component('category-list', CategoryList);
+Vue.component('log-screen', LogScreen);
 
 function loadStories() {
   // You can require as many stories as you need.
   require('../src/components/MyButton/MyButton.story');
   require('../src/components/Modal/Modal.story');
   require('../src/components/CategoryList/CategoryList.story');
+  require('../src/components/LogScreen/LogScreen.story');
 }
 
 configure(loadStories, module);
---
+++ src/components/LogScreen/LogScreen.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  Log Screen
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LogScreen",
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
