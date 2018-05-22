d037a12f31d67760db73a1e3a31972c2974d208a
K.G. O'Hearon <ohearonkg@gmail.com>
2018-05-21 23:05:55 -0300

    basic table to display data now working.

--- src/components/LogScreen/LogScreen.vue
+++ src/components/LogScreen/LogScreen.vue
@@ -22,6 +22,26 @@
       </div>
     </div>
 
+    <!-- Entered Data -->
+    <table class="log-screen__records-table">
+      <tr class="log-screen__record-row" v-for="(record,index) in this.recorded" v-bind:key="record.id">
+        <td v-if="record.notes" class="log-screen__record-entry log-screen__record-entry--text-align-left"> 
+          <svg class="log-screen__notes-icon log-screen__notes-icon--blue-fill" width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1528 1280h-248v248q29-10 41-22l185-185q12-12 22-41zm-280-128h288v-896h-1280v1280h896v-288q0-40 28-68t68-28zm416-928v1024q0 40-20 88t-48 76l-184 184q-28 28-76 48t-88 20h-1024q-40 0-68-28t-28-68v-1344q0-40 28-68t68-28h1344q40 0 68 28t28 68z"/>
+          </svg>
+        </td>
+        <td v-else class="log-screen__record-entry log-screen__record-entry--text-align-left"> 
+          <svg class="log-screen__notes-icon" width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1528 1280h-248v248q29-10 41-22l185-185q12-12 22-41zm-280-128h288v-896h-1280v1280h896v-288q0-40 28-68t68-28zm416-928v1024q0 40-20 88t-48 76l-184 184q-28 28-76 48t-88 20h-1024q-40 0-68-28t-28-68v-1344q0-40 28-68t68-28h1344q40 0 68 28t28 68z"/>
+          </svg>
+        </td>
+
+        <td class="log-screen__record-entry"> {{ index + 1 }} </td>
+        <td class="log-screen__record-entry"> {{ record.weight }} lbs</td>
+        <td class="log-screen__record-entry"> {{ record.reps }} reps </td>
+      </tr>
+    </table>
+
   </div>
 </template>
 
@@ -31,7 +51,30 @@ export default {
   name: "LogScreen",
   components: {
     MyButton
-  }
+  },
+  data: () => ({
+    recorded: [
+      {
+        reps: 2,
+        weight: 55,
+        notes: "Felt Heavy",
+        id: 1
+      },
+
+      {
+        reps: 3,
+        weight: 45,
+        notes: null,
+        id: 2
+      },
+      {
+        reps: 4,
+        weight: 35,
+        notes: "Litte too easy",
+        id: 3
+      },
+    ]
+  })
 }
 </script>
 
@@ -80,6 +123,36 @@ export default {
     &:focus {
       outline: none;
       border-bottom: 2px solid blue;
+    }
+  }
+
+  &__records-table {
+    width: calc(100% - 80px);
+    margin: 0 40px;
+    border-collapse: collapse;
+  }
+
+  &__record-row {
+    padding: 20px 0;
+    border-bottom: 0.5px solid grey;
+  }
+
+  &__record-entry {
+    text-align: right;
+    padding: 10px 0;
+
+    &--text-align-left {
+      text-align: left;
+    }
+  }
+
+  &__notes-icon {
+    width: 20px;
+    height: 20px;
+
+    &--blue-fill {
+      fill: blue;
+      cursor: pointer;
     }
   }
 }
